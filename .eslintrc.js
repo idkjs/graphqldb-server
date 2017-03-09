6f04b3397a08276677075896b2c9ae04ab9849ee
nodkz <pavel.chertorogov@gmail.com>
2017-03-09 12:43:01 +0600

    Cleanup package.json from unused packages. Remove graphql-compose-relay for simplicity, in future it may be easily added. Setup babel in latest recommended way via babel-env (also add it to regular dependencies, because we do not setup build scripts for azure, which will understand import and other es6 shugar).

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,32 @@
 module.exports = {
+  "parser": "babel-eslint",
+  "extends": [
+    "airbnb-base",
+    "prettier"
+  ],
+  "plugins": [
+    "import",
+    "prettier"
+  ],
   "env": {
     "node": true,
-    "es6": true,
-    "jest": true,
   },
-  "parser": "babel-eslint",
-  "extends": "airbnb-base",
   "rules": {
     "import/no-unresolved": [2, {commonjs: true, amd: true}],
     "no-underscore-dangle": 0,
     "arrow-body-style": 0,
     "import/no-extraneous-dependencies": 0,
     "import/imports-first": 0,
-    "no-console":0
+    "no-console": 0,
+    "import/prefer-default-export": 0,
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "ignore",
+    }],
+    "no-param-reassign": 0,
+    "no-use-before-define": 0
   }
 };
