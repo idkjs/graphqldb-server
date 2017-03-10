f40c6d123dee721309dda5434124a909486d667d
Alain Armand <aarmand.inbox@gmail.com>
2017-03-10 16:44:48 +0100

    updated mongoUri

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 export const expressPort = process.env.PORT || 3000;
-// local
-// export const mongoUri = 'mongodb://localhost:27017/yelpdataset';
 
-// azure vm
 export const mongoUri = 'mongodb://localhost:27017/graphqldb';
